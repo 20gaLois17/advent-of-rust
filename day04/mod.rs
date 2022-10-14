1529bf0db989e6549d8c5feb0b623fbf2ec78dc7
sk <sk@bitsandlikes.de>
2022-10-14 23:28:29 +0200

    day04 part two

--- day04/mod.rs
+++ day04/mod.rs
@@ -1,3 +1,5 @@
+use regex::Regex;
+
 pub fn part_one(input: &str) -> i64 {
     let mut result: i64 = 0;
     for line in input.trim().split("\n\n") {
@@ -13,9 +15,7 @@ pub fn part_one(input: &str) -> i64 {
                 }
             }
         }
-        if inc {
-            result += 1;
-        }
+        result += if inc {1} else {0}
     }
     println!("day04 -> part one: {}", result);
     return result;
@@ -23,21 +23,21 @@ pub fn part_one(input: &str) -> i64 {
 
 pub fn part_two(input: &str) -> i64 {
     let mut result: i64 = 0;
-    let mut inc: bool = true;
     for line in input.trim().split("\n\n") {
+        let mut inc: bool = true;
         let fields:Vec<&str> = line.split_whitespace().collect();
-
-        if fields.len() < 7 {
-            continue;
+        let fields_count = fields.len();
+        if fields_count < 7 {
+            inc = false;
         }
         for field in fields {
-            inc = inc & is_valid(field); // only short circuit after testing is done
+            inc = inc & is_valid_field(field, fields_count);
         }
-        if inc {
-            result += 1;
-        }
+        result += if inc {1} else {0};
     }
-    return 0;
+
+    println!("day04 -> part two: {}", result);
+    return result;
 }
 
 enum Field {
@@ -64,46 +64,61 @@ impl Field {
             Field::CountryId      => "cid",
         }
     }
+
+    fn is_valid(&self, val: &str) -> bool {
+        match self {
+            Field::CountryId      => true, // this field does not need to be checked ever
+            Field::Birthyear      => in_range_of(val, 1920, 2002),
+            Field::Issueyear      => in_range_of(val, 2010, 2020),
+            Field::ExpirationYear => in_range_of(val, 2020, 2030),
+            Field::EyeColor       => ["amb", "blu", "brn", "gry", "grn", "hzl", "oth"].iter().any(|e| e == &val),
+            Field::PassportId     => val.chars().count() == 9 && val.chars().all(|c| c.is_digit(10)),
+            Field::HairColor      => Regex::new(r"^#[a-f0-9]{6}$").unwrap().is_match(val),
+            Field::Height         => {
+                let unit = &val[val.len()-2..val.len()];
+                if unit == "cm" {
+                    return in_range_of(&val[..val.len()-2], 150, 193);
+                } else if unit == "in" {
+                    return in_range_of(&val[..val.len()-2], 59, 76);
+                }
+                return false;
+            },
+        }
+    }
 }
 
 // helper functions
-fn is_valid(field: &str) -> bool {
+fn is_valid_field(field: &str, field_count: usize) -> bool {
     let sides: Vec<&str> = field.split(":").collect();
+    let field_name  = sides[0];
+    let field_value = sides[1];
 
     // we could probably use 'match' for this but I don't know how yet
-    if sides[0] == Field::Birthyear.as_str() {
-        if !digits_only(sides[1]) {
-            return false;
-        } else {
-            return in_range_of(sides[1], 1920, 2002);
-        }
+    if field_name == Field::Birthyear.as_str() {
+        return Field::Birthyear.is_valid(field_value);
     }
-    else if sides[0] == Field::Issueyear.as_str() {
-        if !digits_only(sides[1]) {
-            return false;
-        } else {
-            return in_range_of(sides[1], 2010, 2020);
-        }
+    else if field_name == Field::Issueyear.as_str() {
+        return Field::Issueyear.is_valid(field_value);
     }
-    // TODO: implement
-    else if sides[0] == Field::ExpirationYear.as_str() {
+    else if field_name == Field::ExpirationYear.as_str() {
+        return Field::ExpirationYear.is_valid(field_value);
     }
-    // TODO: implement
-    else if sides[0] == Field::Height.as_str() {
+    else if field_name == Field::Height.as_str() {
+        return Field::Height.is_valid(field_value);
     }
-    // TODO: implement
-    else if sides[0] == Field::HairColor.as_str() {
+    else if field_name == Field::HairColor.as_str() {
+        return Field::HairColor.is_valid(field_value);
     }
-    // TODO: implement
-    else if sides[0] == Field::EyeColor.as_str() {
+    else if field_name == Field::EyeColor.as_str() {
+        return Field::EyeColor.is_valid(field_value);
     }
-    // TODO: implement
-    else if sides[0] == Field::PassportId.as_str() {
+    else if field_name == Field::PassportId.as_str() {
+        return Field::PassportId.is_valid(field_value);
     }
-    // TODO: implement
-    else if sides[0] == Field::CountryId.as_str() {
+    else if field_name == Field::CountryId.as_str() {
+        return field_count == 8;
     }
-    return false;
+    panic!("{} did not match any field", field);
 }
 
 fn digits_only(val: &str) -> bool {
@@ -115,8 +130,11 @@ fn digits_only(val: &str) -> bool {
     return true;
 }
 fn in_range_of(val: &str, lb: i32, ub: i32) -> bool {
+    if !digits_only(val) {
+        return false;
+    }
     match val.parse::<i32>() {
-        Err(_) => return false,
+        Err(_)  => return false,
         Ok(num) => return num >= lb && num <= ub,
     }
 }
@@ -130,7 +148,8 @@ mod tests {
     }
     #[test]
     fn part_two() {
-        assert_eq!(super::part_two(include_str!("testinput")), 1);
+        assert_eq!(super::part_two(include_str!("testinput_invalid")), 0);
+        assert_eq!(super::part_two(include_str!("testinput_valid")), 4);
     }
 
     // unit tests
@@ -142,7 +161,6 @@ mod tests {
         assert_eq!(super::digits_only(""), true);
         assert_eq!(super::digits_only("-ab10"), false);
     }
-
     #[test]
     fn in_range_of() {
         assert_eq!(super::in_range_of("30", 30, 42), true);
@@ -151,4 +169,42 @@ mod tests {
         assert_eq!(super::in_range_of("28", 30, 42), false);
         assert_eq!(super::in_range_of("45", 30, 42), false);
     }
+    #[test]
+    fn test_birthyear() {
+        assert!(super::Field::Birthyear.is_valid("1999"));
+        assert!(super::Field::Birthyear.is_valid("2000"));
+        assert!(!super::Field::Birthyear.is_valid("2003"));
+        assert!(!super::Field::Birthyear.is_valid(""));
+    }
+    #[test]
+    fn test_eyecolor() {
+        assert!(super::Field::EyeColor.is_valid("amb"));
+        assert!(super::Field::EyeColor.is_valid("blu"));
+        assert!(!super::Field::EyeColor.is_valid("test"));
+        assert!(!super::Field::EyeColor.is_valid("grey"));
+    }
+    #[test]
+    fn test_height() {
+        assert!(super::Field::Height.is_valid("60in"));
+        assert!(super::Field::Height.is_valid("190cm"));
+        assert!(!super::Field::Height.is_valid("190in"));
+        assert!(!super::Field::Height.is_valid("190"));
+        assert!(!super::Field::Height.is_valid("60"));
+    }
+    #[test]
+    fn test_passport_id() {
+        assert!(super::Field::PassportId.is_valid("000000001"));
+        assert!(super::Field::PassportId.is_valid("001302091"));
+        assert!(super::Field::PassportId.is_valid("000000121"));
+        assert!(!super::Field::PassportId.is_valid("0000000A1"));
+        assert!(!super::Field::PassportId.is_valid("0000000001"));
+    }
+    #[test]
+    fn test_haircolor() {
+        assert!(super::Field::HairColor.is_valid("#123abc"));
+        assert!(super::Field::HairColor.is_valid("#efefef"));
+        assert!(!super::Field::HairColor.is_valid("#123abz"));
+        assert!(!super::Field::HairColor.is_valid("123abc"));
+    }
+
 }
