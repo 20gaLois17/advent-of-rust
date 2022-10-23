6e02a48872660b4be51c54a622ea1a2be6055180
sk <sk@bitsandlikes.de>
2022-10-23 16:49:50 +0200

    day11 part two

--- day11/mod.rs
+++ day11/mod.rs
@@ -13,6 +13,7 @@ pub fn part_one(input: &str) -> usize {
             }
         }
     }
+
     while updates.len() > 0 {
         iterations += 1;
         for update in updates.iter() {
@@ -26,13 +27,13 @@ pub fn part_one(input: &str) -> usize {
             }
         }
 
-        // for line in &board {
-        //     for c in line {
-        //         print!("{}", c);
-        //     }
-        //     print!("\n");
-        // }
-        // println!();
+        //for line in &board {
+        //    for c in line {
+        //        print!("{}", c);
+        //    }
+        //    print!("\n");
+        //}
+        //println!();
 
         updates = vec![];
         for (i, row) in board.iter().enumerate() {
@@ -48,6 +49,7 @@ pub fn part_one(input: &str) -> usize {
     println!("day11 -> part one: {}", result);
     return result;
 }
+
 
 fn count_occupied_seats(board: &Vec<Vec<char>>) -> usize {
     let mut result: usize = 0;
@@ -65,10 +67,81 @@ fn count_occupied_seats(board: &Vec<Vec<char>>) -> usize {
 }
 
 fn will_change(board: &Vec<Vec<char>>, i: usize, k: usize) -> bool {
+    match board[i][k] {
+        '.' => false,
+        'L' => count_surrounding_occupied_seats(&board, i, k) == 0,
+        '#' => count_surrounding_occupied_seats(&board, i, k) > 3,
+        _   => panic!("unknown board character")
+    }
+}
+
+pub fn part_two(input: &str) -> usize {
+    let mut board: Vec<Vec<char>> = input.trim().split('\n')
+        .map(|s| s.chars().collect())
+        .collect();
+
+    // setup and first run
+    let mut iterations: usize = 1;
+    let mut updates: Vec<[usize; 2]> = vec![];
+    for (i, row) in board.iter().enumerate() {
+        for (k, col) in row.iter().enumerate() {
+            if will_change_two(&board, i, k) {
+                updates.push([i, k]);
+            }
+        }
+    }
+    //for i in 0..1 {
+    while updates.len() > 0 {
+        iterations += 1;
+        for update in updates.iter() {
+            let row_index: usize = update[0];
+            let col_index: usize = update[1];
+            match board[row_index][col_index] {
+                'L' => board[row_index][col_index] = '#',
+                '#' => board[row_index][col_index] = 'L',
+                _   => panic!("unidentifiable board char")
+
+            }
+        }
+
+        //for line in &board {
+        //    for c in line {
+        //        print!("{}", c);
+        //    }
+        //    print!("\n");
+        //}
+        //println!();
+
+        updates = vec![];
+        for (i, row) in board.iter().enumerate() {
+            for (k, col) in row.iter().enumerate() {
+                if will_change_two(&board, i, k) {
+                    updates.push([i, k]);
+                }
+            }
+        }
+    }
+    let result: usize = count_occupied_seats(&board);
+
+    println!("day11 -> part one: {}", result);
+    return result;
+}
+
+fn will_change_two(board: &Vec<Vec<char>>, i: usize, k: usize) -> bool {
+    match board[i][k] {
+        '.' => false,
+        'L' => count_visible_occupied_seats(&board, i, k) == 0,
+        '#' => count_visible_occupied_seats(&board, i, k) > 4,
+        _   => panic!("unknown board character")
+    }
+}
+
+fn count_surrounding_occupied_seats(board: &Vec<Vec<char>>, i: usize, k: usize) -> usize {
     let board_rows: i32 = board.len().try_into().unwrap();
     let board_cols: i32 = board[0].len().try_into().unwrap();
     let row_index: i32 = i.try_into().unwrap();
     let col_index: i32 = k.try_into().unwrap();
+
     let offsets: Vec<[i32; 2]> = vec![
         [-1, 0],   // top
         [0, -1],   // left
@@ -79,55 +152,92 @@ fn will_change(board: &Vec<Vec<char>>, i: usize, k: usize) -> bool {
         [1, -1],   // bottom left
         [1, 1]     // bottom right
     ];
+    let mut seat_count: usize = 0;
+    for offset in offsets {
+        let x = row_index + offset[0];
+        let y = col_index + offset[1];
+        if !(x >= 0 && x < board_rows) {
+            continue;
+        }
+        if !(y >= 0 && y < board_cols) {
+            continue;
+        }
+        match board[x as usize][y as usize] {
+            '.' => {},
+            'L' => {},
+            '#' => seat_count += 1,
+            _   => panic!("unidentifiable symbol")
 
-    match board[i][k] {
-        '.' => false,
-        'L' => {
-            // check if seat will get occupied
-            // there must be no occupied seats nearby
-            for offset in offsets {
-                let x = row_index + offset[0];
-                let y = col_index + offset[1];
-                if !(x >= 0 && x < board_rows) {
-                    continue;
-                } else if !(y >= 0 && y < board_cols) {
-                    continue;
-                }
-                if board[x as usize][y as usize] == '#' {
-                    return false;
-                }
-            }
-            return true;
-        },
-        '#' => {
-            // check if seat will get empty
-            // there must be 4 or more occupied seats nearby
-            let mut seat_count: usize = 0;
-            for offset in offsets {
-                let x = row_index + offset[0];
-                let y = col_index + offset[1];
-                if !(x >= 0 && x < board_rows) {
-                    continue;
-                }
-                if !(y >= 0 && y < board_cols) {
-                    continue;
-                }
-                match board[x as usize][y as usize] {
-                    '.' => {},
-                    'L' => {},
-                    '#' => seat_count += 1,
-                    _   => panic!("unidentifiable symbol")
-
-                }
-            }
-            return seat_count >= 4;
-        },
-        _   => panic!("unknown board character")
+        }
     }
+    return seat_count;
 }
-pub fn part_two(input: &str) -> usize {
-    return 0;
+
+fn count_visible_occupied_seats(board: &Vec<Vec<char>>, i: usize, k: usize) -> usize {
+    let board_rows: i32 = board.len().try_into().unwrap();
+    let board_cols: i32 = board[0].len().try_into().unwrap();
+    let row_index: i32 = i.try_into().unwrap();
+    let col_index: i32 = k.try_into().unwrap();
+
+    let offsets: Vec<[i32; 2]> = vec![
+        [-1, 0],   // top
+        [0, -1],   // left
+        [1, 0],    // bottom
+        [0, 1],    // right
+        [-1, -1],  // top left
+        [-1, 1],   // top right
+        [1, -1],   // bottom left
+        [1, 1]     // bottom right
+    ];
+    let mut seat_count: usize = 0;
+    for offset in offsets {
+        let x = row_index + offset[0];
+        let y = col_index + offset[1];
+        if !(x >= 0 && x < board_rows) {
+            continue;
+        }
+        if !(y >= 0 && y < board_cols) {
+            continue;
+        }
+        match board[x as usize][y as usize] {
+            '.' => if found_occupied_in_direction(&board, i ,k, offset) {
+                seat_count += 1;
+            },
+            'L' => {},
+            '#' => seat_count += 1,
+            _   => panic!("unidentifiable symbol")
+
+        }
+    }
+    return seat_count;
 }
+
+pub fn found_occupied_in_direction(board: &Vec<Vec<char>>, i: usize, k: usize, offset: [i32; 2]) -> bool {
+    let board_rows: i32 = board.len().try_into().unwrap();
+    let board_cols: i32 = board[0].len().try_into().unwrap();
+    let row_index: i32 = i.try_into().unwrap();
+    let col_index: i32 = k.try_into().unwrap();
+
+    let mut x = row_index + offset[0];
+    let mut y = col_index + offset[1];
+
+    while board[x as usize][y as usize] == '.' {
+        x  += offset[0];
+        y  += offset[1];
+
+        if !(x >= 0 && x < board_rows) {
+            return false;
+        }
+        if !(y >= 0 && y < board_cols) {
+            return false;
+        }
+    }
+
+    return board[x as usize][y as usize] == '#'
+
+}
+
+
 
 #[cfg(test)]
 mod tests {
@@ -137,6 +247,6 @@ mod tests {
     }
     #[test]
     fn part_two() {
-        assert_eq!(super::part_two(include_str!("testinput")), 1);
+        assert_eq!(super::part_two(include_str!("testinput")), 26);
     }
 }
